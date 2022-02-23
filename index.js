102214e8d4aba0c7147cc3df817dd7595e3bc7f6
Valencia White <valenciawhite@rocketmail.com>
2022-02-22 19:08:09 -0500

    commit tree 7e0b7ddd848130c4e964016f47cf12469a4d2b86

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-// Write your code in this file!
-const currentUser = "Valencia White";
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-
-const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
